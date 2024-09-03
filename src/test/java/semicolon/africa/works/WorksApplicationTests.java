15c6350c8f96880e8c19feef86e977b022c5026a
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-09-03 11:41:24 +0100

    update

---
+++ src/test/java/semicolon/africa/works/WorksApplicationTests.java
@@ -0,0 +1,13 @@
+package semicolon.africa.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
